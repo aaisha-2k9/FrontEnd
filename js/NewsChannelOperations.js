5d67366f3a037d3c2f87e3ee4a0d663942ab724e
Aaisha Parveen <Aaisha_Parveen@epam.com>
2019-07-19 10:35:24 +0530

    Review comments has been addressed

--- js/NewsChannelOperations.js
+++ js/NewsChannelOperations.js
@@ -1,44 +1,64 @@
-function SaveEmail(){
-  var emailAddresss= document.getElementById('txtEmailAddress').value;
-  if (ValidateEmail(emailAddresss)){
-    localStorage.setItem('email',emailAddresss);
+function saveEmail() {
+  var emailAddresss = document.getElementById("txtEmailAddress").value;
+  if (validateEmail(emailAddresss)) {
+    localStorage.setItem("email", emailAddresss);
+    document.getElementById("customerror").innerHTML = "Your Email Address has been saved";
+    document.getElementById("customerror").setAttribute("class","success-message");
+  }
+  else {
+    document.getElementById("customerror").innerHTML =  "Please enter valid Email Address";
+  document.getElementById("customerror").setAttribute("class","custom-error");
   }
 }
-function ValidateEmail(mail)
-{
-  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
-  {
+function validateEmail(mail) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
     return true;
   }
 }
-function ShowHideDiv(selectedElement) {
-  if(selectedElement.value> 0){
-    for(var index =1 ;index< newsChannels.length; index++){
+function showHideDiv(selectedElement) {
 
-      if (selectedElement.value == index){
-        document.getElementById('Channel' +index ).setAttribute('style','display:block');
+  if (selectedElement.value > 0) {
+    for (let index = 1; index < newsChannels.length; index++) {
+      if (selectedElement.value == index) {
+        document
+          .getElementById("Channel" + index)
+          .setAttribute("style", "display:block");
+
+      } else {
+        document
+          .getElementById("Channel" + index)
+          .setAttribute("style", "display:None");
       }
-      else{
-        document.getElementById('Channel' +index).setAttribute('style','display:None');
-      }
-    }}
-    else {
-      for(var index =1 ;index< newsChannels.length; index++){
-        document.getElementById('Channel' +index ).setAttribute('style','display:block');
-      }
+    }
+  } else {
+    newsChannels.forEach((item,index)=> {
+      if (index > 0 ) {
+      document
+        .getElementsByClassName("news-container")[index-1]
+        .setAttribute("style", "display:block");
+    }});
+  }
+}
+
+function showCustomPopup(item) {
+  var selectedDiv = document.getElementById(item);
+  item.parentElement.parentElement.parentElement.setAttribute(
+    "class",
+    "custom-popup"
+  );
+  item.parentElement.setAttribute("style", "display:None");
+  for (let index = 1; index < newsChannels.length; index++) {
+    if (
+      item.parentElement.parentElement.parentElement.id !=
+      document.getElementById("Channel" + index).id
+    ) {
+      document
+        .getElementById("Channel" + index)
+        .setAttribute("class", "news-container");
+      document
+        .getElementById("btnContinue" + index)
+        .setAttribute("style", "display:block");
     }
   }
 
-  function ShowCustomPopup(item)
-  {
-    var selectedDiv= document.getElementById(item);
-    item.parentElement.parentElement.parentElement.setAttribute('class','custom-popup');
-    item.parentElement.setAttribute('style','display:None');
-    for(var index =1 ;index< newsChannels.length; index++){
-            if (item.parentElement.parentElement.parentElement.id != document.getElementById('Channel' +index).id){
-        document.getElementById('Channel' +index).setAttribute('class','news-container');
-        document.getElementById('btnContinue' +index).setAttribute('style','display:block');
-      }
-    }
-
-  }
+}
